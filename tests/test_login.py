805b7442426135040e04705b3045e58b6322d680
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2024-10-05 16:33:14 +0530

    Updated test_login.py file

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,13 +1,4 @@
-from playwright.sync_api import Page, expect
-def test_login(page: Page, logger):
-    username, password = "Admin", "admin123"
-    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+def test_login(logger, login_to_app):
     logger.info("Successfully navigated to OrangeHRM landing page")
-    page.get_by_placeholder("Username").fill("Admin")
-    page.get_by_placeholder("Password").fill("admin123")
-    page.get_by_role("button", name="Login").click()
-    page.wait_for_selector("//span[@class='oxd-userdropdown-tab']")
-    if page.locator("//span[@class='oxd-userdropdown-tab']").is_visible():
-        logger.info(f"Logged in successfully with {username} & {password}.")
-    else:
-        logger.error(f"Error logging in with {username} & {password}")
+    logger.info("Dashboard Link is visible.")
