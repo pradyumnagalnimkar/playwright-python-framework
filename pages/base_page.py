9b605f963d76a101e14fe4fb97b0c86f9a1059c7
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2024-10-05 16:33:42 +0530

    Added side bar locators in base page file

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,6 +5,10 @@ class BasePage:
     def __init__(self, page: Page):
         self.page = page
 
+        """WebElement Locators"""
+        self.dashboard_link = self.page.locator("//span[text()='Dashboard']")
+        self.leave_link = self.page.locator("//span[text()='Leave']")
+
     def load(self):
         self.page.goto(self.url)
 
