9415a8887fa64c3416cc0de992c2d8c8af05c546
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2024-10-05 16:19:13 +0530

    Created BasePage & LoginPage classes as per POM norm

---
+++ pages/base_page.py
@@ -0,0 +1,11 @@
+import os
+from playwright.sync_api import Page
+
+class BasePage:
+    def __init__(self, page: Page):
+        self.page = page
+
+    def load(self):
+        self.page.goto(self.url)
+
+
---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+import os
+
+from pages.base_page import  BasePage
+
+class LoginPage(BasePage):
+    def __init__(self, page):
+        super().__init__(page)
+        self.url = "/"
+
+        """WebElement Locators"""
+        self.username_textbox = self.page.get_by_placeholder("Username")
+        self.password_textbox = self.page.get_by_placeholder("Password")
+        self.login_button = self.page.get_by_role("button", name="Login")
+
+    def login(self, username, password):
+        self.username_textbox.wait_for()
+        self.username_textbox.fill(username)
+        self.password_textbox.fill(password)
+        self.login_button.click()
