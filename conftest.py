efb91f8620494ec1d2c94a5628d1868b6624faf1
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2024-10-05 16:18:43 +0530

    Added login_to_app fixture with autouse=True

--- conftest.py
+++ conftest.py
@@ -1,6 +1,16 @@
+import os
 import pytest
+
+from pages.login_page import LoginPage
 from utils.logger.logger import Logger
 
 @pytest.fixture(scope="session")
 def logger():
     return Logger()
+
+@pytest.fixture(autouse=True)
+def login_to_app(page):
+    login_page = LoginPage(page)
+    login_page.load()
+    login_page.login(os.getenv('USERNAME'), os.getenv('PASSWORD'))
+
